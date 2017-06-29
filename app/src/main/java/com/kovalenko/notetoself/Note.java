fe32ee35ef5db104a4645a1d55e50767af974a34
bananabeard <kostikololo@gmail.com>
2017-06-28 23:29:42 -0400

    Initial commit

---
+++ app/src/main/java/com/kovalenko/notetoself/Note.java
@@ -0,0 +1,10 @@
+package com.kovalenko.notetoself;
+
+public class Note {
+
+    private String mTitle;
+    private String mDescription;
+    private boolean mIdea;
+    private boolean mTodo;
+    private boolean mImportant;
+}
